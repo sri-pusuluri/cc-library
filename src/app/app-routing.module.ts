3ca3dac241e6f93e7203f6f7fdac04870a4cc75f
sri-pusuluri <srini@zool.in>
2023-02-09 22:44:19 +0530

    3 files modified, 4 added, 5 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GroupsComponent } from './cards/groups/groups.component';
-import { LibraryCardV2Component } from './cards/library-card-v2/library-card-v2.component';
 import { LibraryCardComponent } from './cards/library-card/library-card.component';
+import { LibraryCardV2Component } from './cards/librarycard-v2/librarycard-v2.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: 'groups', component:GroupsComponent},
-    {path: 'librarycard', component:LibraryCardComponent},
-    {path: 'librarycardv2', component:LibraryCardV2Component},
-    {path: 'home', component:AppComponent},
-    {path:'**', redirectTo:'home'}
-  ])],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot([
+      { path: 'groups', component: GroupsComponent },
+      { path: 'librarycard', component: LibraryCardComponent },
+      { path: 'librarycardv2', component: LibraryCardV2Component },
+      { path: 'home', component: AppComponent },
+      { path: '**', redirectTo: 'home' },
+    ]),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { GroupsComponent } from './cards/groups/groups.component';
-import { LibraryCardV2Component } from './cards/library-card-v2/library-card-v2.component';
 import { LibraryCardComponent } from './cards/library-card/library-card.component';
+import { AppRoutingModule } from './app.routing.module';
+import { LibraryCardV2Component } from './cards/librarycard-v2/librarycard-v2.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      GroupsComponent,
-      LibraryCardComponent,
-      LibraryCardV2Component,
-      CardsComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    GroupsComponent,
+    LibraryCardComponent,
+    LibraryCardV2Component,
+    CardsComponent,
   ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
